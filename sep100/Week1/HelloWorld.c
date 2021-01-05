9555e891663da3a12e3b343d2a284814fcee4f04
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-01-05 14:12:00 -0500

    Added more lecture material to week 1 of sep100.

---
+++ sep100/Week1/HelloWorld.c
@@ -0,0 +1,11 @@
+//HelloWorld.c - A simple hello world application
+
+/* This is documentation */
+
+#include <stdio.h>//Include the standard input-output library
+
+int main(void)//All programs start from main()
+{
+	printf("Hello World\n");//Prints to the screen
+	return 0;//main() returns 0, this means no error
+}
