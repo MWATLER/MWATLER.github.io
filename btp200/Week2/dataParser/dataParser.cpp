82082249dc80961c065f5e9058b102059c92f0d0
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-01-18 13:34:52 -0500

    Included link to C++ Primer on the btp200 webpage. Documented dataParser.cpp for workshop 1 for btp200.

--- btp200/Week2/dataParser/dataParser.cpp
+++ btp200/Week2/dataParser/dataParser.cpp
@@ -9,17 +9,28 @@ const int BUF_LEN = 32;
 int main(void) {
 	//A data stream where the first character represents
 	//the length of the data to be copied
-	char* data=(char *)"9introducingBTP200";
-	char* dataPtr = data;
-	char  destPtr[BUF_LEN];
+	const char* data="9introducingBTP200";//9 is metadata, it represents the length
+	                                      //of data to be copied
+	const char* dataPtr = data;//dataPtr[0]='9'
+	char  dest[BUF_LEN];
 
-	int len = dataPtr[0]-'0';//extract the length
+	int len = dataPtr[0] - '0';//extract the length
+//	int len = *dataPtr - '0';//extract the length
 	int i;
 	for (i = 0; i < len && i<(BUF_LEN-1) && dataPtr[i]!='\0'; ++i) {
-		destPtr[i] = dataPtr[i + 1];
+		dest[i] = dataPtr[i + 1];//Why i+1 ? Because we skip the first character
+		//dest[0] = dataPtr[1] = 'i'
+		//dest[1] = dataPtr[2] = 'n'
+		//dest[2] = dataPtr[3] = 't'
+		//dest[3] = dataPtr[4] = 'r'
+		//dest[4] = dataPtr[5] = 'o'
+		//dest[5] = dataPtr[6] = 'd'
+		//dest[6] = dataPtr[7] = 'u'
+		//dest[7] = dataPtr[8] = 'c'
+		//dest[8] = dataPtr[9] = 'i'
 	}
-	destPtr[i] = '\0';
-	cout << destPtr << endl;
+	dest[i] = '\0';//If len is 9, what is "i" here? i is 9
+	cout << dest << endl;//dest="introduci"
 
 	return 0;
 }
