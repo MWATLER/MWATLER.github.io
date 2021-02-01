bdf01ba08b763c23d293dca009de93192afc3f58
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-01-31 20:17:00 -0500

    Added sample code for week 4 of btp200

---
+++ btp200/Week4/personalInfo2/personalInfo.h
@@ -0,0 +1,19 @@
+//personalInfo.h - header file for personal information
+//
+// 26-Jan-21  M. Watler         Created.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+class PersonalInfo {
+	char* name;
+	char* address;
+	int age;
+public:
+	PersonalInfo();
+	PersonalInfo(const char* _name, const char* _address, const int _age);
+	~PersonalInfo();
+	bool setInfo(const char* _name, const char* _address, int _age);
+	bool setAddress(const char* _address);
+	void displayInfo() const;
+};
