4eaadf9e30323a937ebaf76557959255bedfde6f
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-02-18 16:35:38 -0500

    Added sample code for week 8 of sep100

---
+++ sep100/Week8/StudentMarks/Student.h
@@ -0,0 +1,18 @@
+//Student.h - structure declaration and function prototypes for student marks
+
+#define BUF_LEN 64
+#define MAX_MARKS 6
+
+#include <stdbool.h>
+
+struct Student {
+	char name[BUF_LEN];
+	int year;
+	int mark[MAX_MARKS];
+	int numMarks;
+	double average;
+};
+
+double CalculateAveragePassByValue(struct Student student);
+bool CalculateAveragePassByAddress(struct Student *student);
+bool CalculateAverages(struct Student *student, int length);
