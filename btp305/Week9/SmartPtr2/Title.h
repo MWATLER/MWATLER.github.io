299592e7c23727364e231230c32e289657f190b0
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-11-05 16:04:13 -0400

    Added lecture notes and sample code for week9 of btp305.

---
+++ btp305/Week9/SmartPtr2/Title.h
@@ -0,0 +1,19 @@
+// Title.h - Smart Pointers
+
+#include <iostream>
+
+class Title {
+    const char* title;
+    const char* validTitle() const {
+        if (!title[0]) throw "invalid title";
+        return title;
+    }
+public:
+    Title(const char* s) : title(s) {}
+    void display() const {
+        std::cout << validTitle() << std::endl;
+    }
+    ~Title() {
+        std::cout << "~Title: destroying title" << std::endl;
+    }
+};
---
+++ btp305/Week9/StoogeFactory/Larry.h
@@ -0,0 +1,21 @@
+//Larry.h - class declaration for the stooge Larry
+
+#ifndef _LARRY_H_
+#define _LARRY_H_
+
+#include "Stooge.h"
+
+class Larry : public Stooge
+{
+public:
+    void slap_stick()
+    {
+        std::cout << "Larry: poke eyes\n";
+    }
+    ~Larry() {
+        std::cout << "~Larry()" << std::endl;
+    }
+
+};
+
+#endif _LARRY_H_
