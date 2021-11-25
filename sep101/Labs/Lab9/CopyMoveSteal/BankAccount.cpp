eaeaa4f3b0d2c86f22600e153d3ddf0fbfdbc33a
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-11-25 11:44:51 -0500

    Updated the sample code for lab9 of sep101. Added link to the lab recording for lab9 of sep101.

--- sep101/Labs/Lab9/CopyMoveSteal/BankAccount.cpp
+++ sep101/Labs/Lab9/CopyMoveSteal/BankAccount.cpp
@@ -30,8 +30,8 @@ double BankAccount::WithdrawMoney(double amount) {
 	return balance;
 }
 
-void BankAccount::CopyAccount(const BankAccount& account) {
-	//skip validation
+void BankAccount::CopyAccount(const BankAccount& account) {//account1.CopyAccount(account2);
+	                                                       //this refers to account1, account refers to account2
 	if (this != &account) {//do not copy to yourself
 		this->name = account.name;
 		this->socialInsNumber = account.socialInsNumber;
@@ -39,18 +39,22 @@ void BankAccount::CopyAccount(const BankAccount& account) {
 	}
 }
 
-void BankAccount::MoveAccount(BankAccount& account) {
+void BankAccount::MoveAccount(BankAccount& account) {//account2.MoveAccount(account3);
+  												     //this refers to account2, account refers to account3
 	if (this != &account) {//do not move to yourself
+		//copy everything over
 		this->name = account.name;
 		this->socialInsNumber = account.socialInsNumber;
 		this->balance = account.balance;
+		//put account into an empty state
 		account.name = "";
 		account.socialInsNumber = 0;
 		account.balance = 0.0;
 	}
 }
 
-void BankAccount::StealAccount(BankAccount& account) {
+void BankAccount::StealAccount(BankAccount& account) {//account1.StealAccount(account2);
+													  //this refers to account1, account refers to account2
 	if (this != &account) {//do not steal from yourself
 		//a good thief would check to see if the balance of his/her victim is positive. No one wants to steal debt
 		if (account.balance > 0) {
