6a79da582a7230d892d914000988d4eb7ab327bf
MWATLER <42724074+MWATLER@users.noreply.github.com>
2021-12-02 12:32:58 -0500

    Added link to the lab recording and updated sample code for lab10 of sep101.

--- sep101/Labs/Lab10/DirectCurrentVoltage/DirectCurrentVoltage.cpp
+++ sep101/Labs/Lab10/DirectCurrentVoltage/DirectCurrentVoltage.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 int main() {
-	Inductor inductor(0.1);
-	const double delta = 0.0001;
-	const double voltage = 10.0;
+	Inductor inductor(0.1);//Inductance of 0.1 Henries
+	const double delta = 0.0001;//timestep of 0.1ms
+	const double voltage = 10.0;//DC voltage of 10Volts
 	ofstream file("L.dat");
 
 	file << setw(8) << "TIME" << setw(8) << "VOLTAGE" << setw(8) << "CURRENT" << endl;
-	for (double time = 0.0; time < 0.1; time += delta) {
+	for (double time = 0.0; time < 0.1; time += delta) {//run for 0.1 seconds
 		double current = inductor.GetCurrent(voltage, delta);
 		file << setw(8) << time << setw(8) << voltage << setw(8) << current << endl;
 	}
