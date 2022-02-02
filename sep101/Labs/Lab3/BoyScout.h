33eafbd6ce761d3b2ed84781b8dcc201a41e6122
MWATLER <42724074+MWATLER@users.noreply.github.com>
2022-02-02 15:10:07 -0500

    Updated lab3 for sep101.

--- sep101/Labs/Lab3/BoyScout.h
+++ sep101/Labs/Lab3/BoyScout.h
@@ -3,7 +3,7 @@
 #define MAX_STR 64
 
 struct BoyScout {
-	char name[MAX_STR];
+	char name[MAX_STR];//holds up to 63 characters plus the null terminator
 	int age;
 	int number;
 };
--- sep101/Labs/Lab3/BoyScoutMain.c
+++ sep101/Labs/Lab3/BoyScoutMain.c
@@ -1,66 +1,61 @@
 //BoyScoutMain.c - main program for boy scouts
 
-#include <stdio.h>//standard C library, this is why we use <   >
-#include "BoyScout.h"//my header file, I use "" for a relative path to the file
+#include <stdio.h>
+#include "BoyScout.h"
 
 int main() {
 	const int SIZE = 5;
+/*	struct BoyScout {
+		char name[MAX_STR];
+		int age;
+		int number;
+	};*/
 	struct BoyScout boyScout[] = {
-    	{"Billy Blackledge", 12, 1000123},
-	    {"Carla Dixon", 6, 1000124},
-    	{"Bashab Patel", 13, 1000125},
-    	{"Erin Mark", 10, 1000126},
-    	{"Mulopo Mbape", 11, 1000127}
+		{"Billy Blackledge", 12, 1000123},//index 0
+		{"Carla Dixon", 6, 1000124},//index 1
+		{"Bashab Patel", 13, 1000125},//index 2
+		{"Erin Mark", 10, 1000126},//index 3
+		{"Mulopo Mbape", 11, 1000127},//index 4
 	};
 
 	int number;
 	printf("Enter the number of someone to remove: ");
 	scanf_s("%d", &number);
-	int found = 0;
-	for (int i = 0; i < SIZE && found==0; ++i) {
+	int found = 0;//the found flag
+	for (int i = 0; i < SIZE && found == 0; ++i) {
 		if (boyScout[i].number == number) {
 			found = 1;
-			boyScout[i].number = 0;
+			boyScout[i].number = 0;//number 0 indicates removal, will invalidate an entire row in the array
 		}
 	}
-	if (found == 0) {
-		printf("Boyscout with number %d not found!\n", number);
-	}
-	printf("\n");
 
+	printf("\n");
 	for (int i = 0; i < SIZE; ++i) {
-		//Print valid boyscouts only
-		if (boyScout[i].number!=0) {
-			printf("%s %d %d\n", 
-				boyScout[i].name, boyScout[i].age, boyScout[i].number);
+		if (boyScout[i].number != 0) {//print out the valid boy scouts
+			printf("%s %d %d\n", boyScout[i].name, boyScout[i].age, boyScout[i].number);
 		}
 	}
-	printf("\n");
 
-	printf("Enter someone new: ");
-	found = 0;
+	printf("\n");
+	printf("Enter someone new\n");
+	found = 0;//look for an empty spot in the array
 	for (int i = 0; i < SIZE && found == 0; ++i) {
 		if (boyScout[i].number == 0) {
 			found = 1;
-			printf("Name: ");
-			scanf_s("%s", boyScout[i].name, MAX_STR);
-			printf("Age: ");
+			printf("name: ");
+			scanf_s("%s", boyScout[i].name, MAX_STR);//can only scan one string, not two
+			printf("age: ");
 			scanf_s("%d", &boyScout[i].age);
-			printf("Number: ");
+			printf("number: ");
 			scanf_s("%d", &boyScout[i].number);
 		}
 	}
-	if (found == 0) {
-		printf("No space in the array for the new boyscout!\n");
-	}
 
 	for (int i = 0; i < SIZE; ++i) {
-		//Print valid boyscouts only
-		if (boyScout[i].number != 0) {
-			printf("%s %d %d\n",
-				boyScout[i].name, boyScout[i].age, boyScout[i].number);
+		if (boyScout[i].number != 0) {//print out the valid boy scouts
+			printf("%s %d %d\n", boyScout[i].name, boyScout[i].age, boyScout[i].number);
 		}
 	}
-	   	  
+
 	return 0;
 }
