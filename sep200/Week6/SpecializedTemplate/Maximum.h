5ce7e2993e9f6869e49d1ee6926cc799e26ebdc7
MWATLER <42724074+MWATLER@users.noreply.github.com>
2022-02-14 20:52:46 -0500

    Added more sample code to week6 of sep200.

---
+++ sep200/Week6/SpecializedTemplate/Maximum.h
@@ -0,0 +1,16 @@
+// Template Specialization
+// Maximum.h
+
+#include <cstring>
+
+template <class T>
+T maximum(T a, T b) {
+    return a > b ? a : b;
+}
+
+// specialization for char* types
+//
+template <> // denotes specialization
+char* maximum<char*>(char* a, char* b) {
+    return std::strcmp(a, b) > 0 ? a : b;
+}
