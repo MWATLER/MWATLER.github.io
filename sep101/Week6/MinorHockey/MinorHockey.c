aec36f40de73bc548e75352cbb8452cfdc0090d6
MWATLER <42724074+MWATLER@users.noreply.github.com>
2022-02-18 13:38:22 -0500

    updated sample code and added link to the lecture recording of week6 of sep101.

--- sep101/Week6/MinorHockey/MinorHockey.c
+++ sep101/Week6/MinorHockey/MinorHockey.c
@@ -1,5 +1,6 @@
 //MinorHockey.c - main code for a minor hockey tournament
 
+#define _CRT_SECURE_NO_WARNINGS
 #define NUM_TEAMS 5
 #include <stdio.h>
 #include "MinorHockey.h"
@@ -11,7 +12,7 @@ int main(void) {
 	printf("\nPlease enter the team names:\n");
 	for(int i = 0; i < NUM_TEAMS; ++i) {
 		printf("Team %d: ", i+1);
-		scanf_s("%[^\n]s", team[i].name, STR_LEN);//scan to the end of the line
+		scanf("%[^\n]s", team[i].name);//scan to the end of the line
 		while (getchar() != '\n');//clear the input buffer ('\n' still remains in the input buffer)
 	}
 
@@ -22,9 +23,9 @@ int main(void) {
 			int goals1, goals2;
 			printf("\n%s vs %s:\n", team[i].name, team[j].name);
 			printf("Enter the goals for %s: ", team[i].name);
-			scanf_s("%d", &goals1);
+			scanf("%d", &goals1);
 			printf("Enter the goals for %s: ", team[j].name);
-			scanf_s("%d", &goals2);
+			scanf("%d", &goals2);
 			//Update team statistics
 			++team[i].gamesPlayed;
 			++team[j].gamesPlayed;
@@ -58,11 +59,9 @@ int main(void) {
 	printf("%s has won the tournament with %d points and a winning percentage of %.2f",
 		team[mostPointsIndex].name, team[mostPointsIndex].points, team[mostPointsIndex].winningPercentage);
 }
-
 void CalculatePoints(struct Team* team) {
 	team->points = 2 * team->wins + team->ties;
 }
-
 void CalculateWinningPercentage(struct Team* team) {
 	int maxPoints = 2 * team->gamesPlayed;
 	if (maxPoints > 0) {
@@ -72,3 +71,16 @@ void CalculateWinningPercentage(struct Team* team) {
 		team->winningPercentage = 0.0;
 	}
 }
+
+/*We want to write software that generates the standings for a minor hockey tournament.
+There are five teams and each team will play four games, playing each other once. The
+team with the most points at the end of the tournament wins. If two teams are tied with
+the most points, those teams will play each other again.
+
+For each team, the team name, games played, the wins, losses, ties, points and winning
+percentage will be tracked. The points will be generated from the wins (2 points for a win)
+and ties (1 point for a tie). The winning percentage will be points divided by maximum
+possible points (2 times games played) times 100%.
+
+The user will be prompted to enter the score for each game by entering the name and goals
+of the first team, then the name and goals of the second team. */
