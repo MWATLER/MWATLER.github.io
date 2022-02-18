f5cc101c1b574bd862957f8e496c1c250d4275a0
MWATLER <42724074+MWATLER@users.noreply.github.com>
2022-02-18 08:50:11 -0500

    Minor modifications to lab5 of sep200.

--- sep200/Labs/Lab5/CompanyMain.cpp
+++ sep200/Labs/Lab5/CompanyMain.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main() {
 	const int NUM = 2;
 	const int MAX_SIZE = 1000;
-	CompanyEmployee standardEmployee;
-	CompanyExecutive executiveEmployee;
+	CompanyEmployee standardEmployeeDB;
+	CompanyExecutive executiveEmployeeDB;
 
 	cout << "Add three persons to each of your databases." << endl;
-	AddPersonnel(standardEmployee, 3);
-	AddPersonnel(executiveEmployee, 3);
+	AddPersonnel(standardEmployeeDB, 3);
+	AddPersonnel(executiveEmployeeDB, 3);
 
 	string name;
 	cout << "Enter a name to search for in both databases: ";
@@ -22,36 +22,36 @@ int main() {
 
 	StandardEmployee sEmpl;
 	ExecutiveEmployee eEmpl;
-	bool found = FindPersonnel(standardEmployee, name, sEmpl);
+	bool found = FindPersonnel(standardEmployeeDB, name, sEmpl);
 	cout.setf(ios::fixed);
 	cout.precision(2);
 	if (found) {
-		cout << sEmpl.name << " works as " << sEmpl.position << " and earned $" << CalculateSalary(standardEmployee, name) << " last year." << endl << endl;
+		cout << sEmpl.name << " works as " << sEmpl.position << " and earned $" << CalculateSalary(standardEmployeeDB, name) << " last year." << endl << endl;
 	}
 	else {
-		found = FindPersonnel(executiveEmployee, name, eEmpl);
+		found = FindPersonnel(executiveEmployeeDB, name, eEmpl);
 		if (found) {
-			cout << eEmpl.name << " works as " << eEmpl.position << " and earned $" << CalculateSalary(executiveEmployee, name) << " last year." << endl << endl;
+			cout << eEmpl.name << " works as " << eEmpl.position << " and earned $" << CalculateSalary(executiveEmployeeDB, name) << " last year." << endl << endl;
 		}
 	}
 
 	cout << "Add two more persons to your databases." << endl;
-	AddPersonnel(standardEmployee, 2);
-	AddPersonnel(executiveEmployee, 2);
+	AddPersonnel(standardEmployeeDB, 2);
+	AddPersonnel(executiveEmployeeDB, 2);
 
 	cout << "Enter another name to search for in both databases: ";
 	getline(std::cin, name);
 
-	found = FindPersonnel(standardEmployee, name, sEmpl);
+	found = FindPersonnel(standardEmployeeDB, name, sEmpl);
 	cout.setf(ios::fixed);
 	cout.precision(2);
 	if (found) {
-		cout << sEmpl.name << " works as " << sEmpl.position << " and earned $" << CalculateSalary(standardEmployee, name) << " last year." << endl << endl;
+		cout << sEmpl.name << " works as " << sEmpl.position << " and earned $" << CalculateSalary(standardEmployeeDB, name) << " last year." << endl << endl;
 	}
 	else {
-		found = FindPersonnel(executiveEmployee, name, eEmpl);
+		found = FindPersonnel(executiveEmployeeDB, name, eEmpl);
 		if (found) {
-			cout << eEmpl.name << " works as " << eEmpl.position << " and earned $" << CalculateSalary(executiveEmployee, name) << " last year." << endl << endl;
+			cout << eEmpl.name << " works as " << eEmpl.position << " and earned $" << CalculateSalary(executiveEmployeeDB, name) << " last year." << endl << endl;
 		}
 	}
 
