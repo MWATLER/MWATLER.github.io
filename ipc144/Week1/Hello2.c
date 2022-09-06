8f9d6c0c3845fbcefcb9639daf7145a17b40f191
MWATLER <42724074+MWATLER@users.noreply.github.com>
2022-09-06 14:06:22 -0400

    Added a second hello world program to week1 of ipc144.

---
+++ ipc144/Week1/Hello2.c
@@ -0,0 +1,11 @@
+//Hello2.c - Another hello world program
+
+//History:
+// 06-Sep-22  M. Watler         Created.
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello world again...\n");
+	return 0;
+}
