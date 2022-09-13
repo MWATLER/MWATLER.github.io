4c7bd8cc441e5d7b6301c82eb666c3c613717c28
MWATLER <42724074+MWATLER@users.noreply.github.com>
2022-09-13 10:36:23 -0400

    Updated sample code for week2 of sep101.

--- sep101/Week2/StudentInformation.c
+++ sep101/Week2/StudentInformation.c
@@ -5,17 +5,27 @@
 
 int main(void)
 {
-	int student_id;
-	char lastName[30];//an array of 30 characters to store the last name.
+	//"Watler": lastName[0]='W'
+	//          lastName[1]='a'
+	//          lastName[2]='t'
+	//          lastName[3]='l'
+	//          lastName[4]='e'
+	//          lastName[5]='r'
+	//          lastName[6]='\0'
+	int studentId;
+	char lastName[30];//up to 29 characters, plus the null terminator \0
 	float mark1;
 	float mark2;
 	float mark3;
 
 	printf("Enter the student's last name: ");
-	scanf("%s", lastName);//no apersand necessary, because the name of the array is the address
-	                      //lastName by itself is actually an address
+	scanf("%s", lastName);//lastName is the address of the top of the array
+	                      //lastName by itself is already an address
+	                      //If I wanted to change just the first
+	                      //character in lastName, then scanf becomes
+	                      //scanf("%c", &lastName[0]);
 	printf("Enter the student's id number: ");
-	scanf("%d", &student_id);
+	scanf("%d", &studentId);
 	printf("Enter the student's first mark: ");
 	scanf("%f", &mark1);
 	printf("Enter the student's second mark: ");
@@ -27,7 +37,7 @@ int main(void)
 /*	printf("Enter the student's last name: ");
 	scanf_s("%s", lastName, 30);
 	printf("Enter the student's id number: ");
-	scanf_s("%d", &student_id);
+	scanf_s("%d", &studentId);
 	printf("Enter the student's first mark: ");
 	scanf_s("%f", &mark1);
 	printf("Enter the student's second mark: ");
@@ -35,8 +45,8 @@ int main(void)
 	printf("Enter the student's third mark: ");
 	scanf_s("%f", &mark3);
 */
-	printf("The student %s with id %d has marks of %f, %f, and %f\n", 
-		lastName, student_id, mark1, mark2, mark3);
+	printf("The student %s with id %d has marks of %f, %f, and %f\n",
+		lastName, studentId, mark1, mark2, mark3);
 
 	return 0;
 }
